3f91a7ca76aab42c2e7acb77259a25d0ff19efad
geo-petrini <geo.petrini@gmail.com>
2025-01-16 15:30:46 +0100

    defined model

--- models/model.py
+++ models/model.py
@@ -1,3 +1,16 @@
-from models.conn import db
+import datetime
 from flask import current_app
 from flask import abort, redirect, url_for, flash
+from sqlalchemy import ForeignKey
+from models.conn import db
+
+class Cloud(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), unique=False, nullable=True)
+    ts_add = db.Column( db.Float(), default=datetime.datetime.now().timestamp() )
+    
+    
+class Word(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    term = db.Column(db.String(200), unique=False, nullable=True)
+    cloud_id = db.Column(db.Integer(), ForeignKey('cloud.id'))
